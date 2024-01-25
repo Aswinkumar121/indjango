0f53fda1a7dc527cafd9e8c277dab68d58013027
Aswinkumar121 <144877602+Aswinkumar121@users.noreply.github.com>
2024-01-25 06:43:25 -0800

    Update settings.py

--- finance_pro/settings.py
+++ finance_pro/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m=t*0%a47c=%&p6h@7i9a!zc1i%lu4mu%irwd988oaek&l&(y$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
